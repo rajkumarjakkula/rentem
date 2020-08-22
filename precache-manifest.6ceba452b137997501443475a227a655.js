c5db90d4e983e46cce79d981a268d43de0ed5a6f
unknown <rajkumarjakkula969@gmail.com>
2020-08-22 17:18:41 +0530

    Updates

---
+++ precache-manifest.6ceba452b137997501443475a227a655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8245c94959a61bc84d0d9a928226fd4",
+    "url": "/rajkumarjakkula/rentem.git/index.html"
+  },
+  {
+    "revision": "d5616f53c4ccda1d2a25",
+    "url": "/rajkumarjakkula/rentem.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "561f676de0baafda1b50",
+    "url": "/rajkumarjakkula/rentem.git/static/js/2.8532016f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rajkumarjakkula/rentem.git/static/js/2.8532016f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5616f53c4ccda1d2a25",
+    "url": "/rajkumarjakkula/rentem.git/static/js/main.c90f8a49.chunk.js"
+  },
+  {
+    "revision": "af6358255e60af57a2c0",
+    "url": "/rajkumarjakkula/rentem.git/static/js/runtime-main.3598c9d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rajkumarjakkula/rentem.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
